697b5c5a76ee4411a5efef146af22dfba3e6e1f2
ggMarcos-ferreira <marcos-ferreira.mf@academico.ifpb.edu.br>
2025-08-19 14:32:55 -0300

    mudança nos comentarios

--- src/cadastrar.js
+++ src/cadastrar.js
@@ -5,9 +5,14 @@ const salvarFruteira = document.getElementById("salvarFruteira");
 function criarCartao(fruteira){
     const dataPlantio = new Date(fruteira.dataPlantio);
     const hoje = new Date();
-    const idadeMeses = (hoje.getFullYear() - dataPlantio.getFullYear()) * 12 + (hoje.getMonth() - dataPlantio.getMonth());
+    //Gerado por IA,subtrai a data de hoje pela data do plantio.
+    const diferencaMs = hoje.getTime() - dataPlantio.getTime();
+    //calcula o numero de milesimos em 1 dia
+    const meses = diferencaMs / (1000 * 60 * 60 * 24 * 30.44);
+    //converte os segundos em meses
+    const idadeMeses = Math.round(meses);
 
-
+//cria uma variavel para armazenar o elemento html e colocar no site baseado nos objetos da lista
     const cartao = `
 <div class="card" style="width: 18rem;">
   <div class="card-body">
@@ -22,6 +27,8 @@ function criarCartao(fruteira){
   </div>
 </div>
 `;
+
+//cria uma div para inserir a variavel cartão nela
     const novaDiv = document.createElement("div");
     novaDiv.classList.add("col-md-4", "mb-3");
     novaDiv.innerHTML = cartao;
@@ -29,6 +36,8 @@ function criarCartao(fruteira){
 
 }
 
+
+//essa função cria a lista com os ids no html.
 salvarFruteira.addEventListener("click", () => {
     const fruteira = {
         id: Date.now(),
@@ -39,11 +48,11 @@ salvarFruteira.addEventListener("click", () => {
         
     };
     
-
+//salva a lista no localstorage, foi necessario o JSON pois o localstorage não salva as listas inteiras.
     let lista = JSON.parse(localStorage.getItem("fruteiras")) || [];
     lista.push(fruteira);
     localStorage.setItem("fruteiras", JSON.stringify(lista));
-
+//chama a função para criar um cartão com os itens da lista
     criarCartao(fruteira)
 
     
@@ -52,7 +61,7 @@ salvarFruteira.addEventListener("click", () => {
     
 });
 
-
+//o codigo só será carregado quando todo o html tbm tiver sido carregado. isso foi necessario pq os itens do localstorage não estavam aparecendo no site, e eunão sabia oq fazer.
 window.addEventListener("DOMContentLoaded", () => {
   let lista = JSON.parse(localStorage.getItem("fruteiras")) || [];
   lista.forEach(fruteira => criarCartao(fruteira));
